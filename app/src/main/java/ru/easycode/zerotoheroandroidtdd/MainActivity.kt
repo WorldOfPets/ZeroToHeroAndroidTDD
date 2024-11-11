48f1496c7b6b2c58350c834ff8366093870570bf
jojo <polivanov004@gmail.com>
2024-11-11 14:28:03 +0300

    solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,24 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 
+private var isDeleted = false
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val container = findViewById<LinearLayout>(R.id.rootLayout)
+        val titleTextView = findViewById<TextView>(R.id.titleTextView)
+        val removeButton = findViewById<TextView>(R.id.removeButton)
+
+        if (isDeleted){
+            container.removeView(titleTextView)
+        }
+        removeButton.setOnClickListener {
+            isDeleted = true
+            container.removeView(titleTextView)
+        }
     }
 }
