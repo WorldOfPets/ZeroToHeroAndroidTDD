841295fb9b67c50869bb2e927102294a1d563a8f
jojo <polivanov004@gmail.com>
2024-11-12 17:58:26 +0300

    solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import java.io.Serializable
 
 class MainActivity : AppCompatActivity() {
     private lateinit var uiState: UiState
