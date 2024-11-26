269d38dcaca5c3827875a5ce575dab292d86e54c
jojo <polivanov004@gmail.com>
2024-11-26 15:01:15 +0300

    solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/SimpleService.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/SimpleService.kt
@@ -3,16 +3,14 @@ package ru.easycode.zerotoheroandroidtdd
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Url
 import java.net.UnknownHostException
 
 interface SimpleService {
-    @GET
-    suspend fun fetch(@Url url: String): SimpleResponse
+    @GET("{urlFrom}")
+    suspend fun fetch(@Path(value = "urlFrom", encoded = true) url: String): SimpleResponse
     class Base:SimpleService{
-        private var expectSuccessResult: Boolean = false
-
-        private lateinit var exceptionToThrow: Exception
 
         private val retrofit = Retrofit.Builder()
             .baseUrl("https://www.google.com/")
